56200854cc3091b5e455886ba5acd0fb88a6029d
Tejas Deshpande <tejas-kiran.deshpande@trivago.com>
2020-03-27 14:54:52 +0100

    setting dyanmic path for sdk

--- flutter_setup.sh
+++ flutter_setup.sh
@@ -1,6 +1,6 @@
 # Setup the sdk and add it to path
-echo "Setting Flutter path: ../test_app/flutter/bin"
-export PATH="$PATH:/Users/tdeshpande/Documents/GitHub/test_app/flutter/bin"
+echo "Setting Flutter path: \$HOME/Documents/GitHub/test_app/flutter/bin"
+export PATH="$PATH:$HOME/Documents/GitHub/test_app/flutter/bin"
 
 # Check if flutter is running
 flutter doctor
