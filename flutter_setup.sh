f021e1b8f78c46d5276c33da2acfa4c26b885588
Tejas Deshpande <tejas-kiran.deshpande@trivago.com>
2020-03-27 10:19:51 +0100

    flutter project started

---
+++ flutter_setup.sh
@@ -0,0 +1,6 @@
+# Setup the sdk and add it to path
+echo "Setting Flutter path: ../test_app/flutter/bin"
+export PATH="$PATH:/Users/tdeshpande/Documents/GitHub/test_app/flutter/bin"
+
+# Check if flutter is running
+flutter doctor
