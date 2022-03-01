d908ff62be5b58d4f14d3e4859ab506b2ff15069
tkdeshpande <tkdeshpande@yahoo.co.in>
2022-03-01 18:59:31 +0100

    base setup

---
+++ config.js
@@ -0,0 +1,51 @@
+"use strict";
+
+module.exports = {
+  url: "https://tkdeshpande.github.io",
+  pathPrefix: "/",
+  title: "Blog by John Doe",
+  subtitle:
+    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "",
+  useKatex: false,
+  menu: [
+    {
+      label: "Articles",
+      path: "/",
+    },
+    {
+      label: "About me",
+      path: "/pages/about",
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts",
+    },
+  ],
+  author: {
+    name: "John Doe",
+    photo: "/photo.jpg",
+    bio: "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+    contacts: {
+      email: "",
+      facebook: "",
+      telegram: "",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+      medium: "",
+    },
+  },
+};
